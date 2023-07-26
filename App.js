96f57f4e17e49bc36bd76811249103c46578b52e
manuannavarapu <manohari.manu135@gmail.com>
2023-07-26 12:05:22 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ * 
+ * 
+ *  */
+
+
+
+
+
+
+
+
+
+const heading = React.createElement("h1", {id : "heading"},"Hello world from React"); 
+
+console.log(heading);  //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
